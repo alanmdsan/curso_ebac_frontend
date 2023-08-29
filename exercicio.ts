af758a74cf0fa2d6d034357024b30a2bfa2a934a
Alan Santos <132408065+alanmdsan@users.noreply.github.com>
2023-08-29 19:32:52 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b
+}
+
+console.log(multiplicacao(10, 12))
+
+function saudacao(nome: string): string {
+    return 'Olá ' + nome + '!'
+}
+
+console.log(saudacao('Alan'))
